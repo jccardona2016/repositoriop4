af3c2f0cb868b76e8c68a9d8cb96a4617fc60ca9
jccardona2016 <jccaguilera@hotmail.com>
2016-11-12 17:22:47 -0600

    modificaion 3
    variable znueva

--- ClaseA.java
+++ ClaseA.java
@@ -1,10 +1,12 @@
 public class ClaseA{
  private String x;
  private int y;
+ public int z;
  
  public claseA(){
   x=¨¨;
   y=0;
+  z=0;
  }
  public void mensaje(){
   System.out.println(¨Hola¨);
